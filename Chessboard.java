5ab4795c05a4bdf276b6cb75a3dae5b975dfb89e
Rich Stone <richard.had@hotmail.de>
2016-07-23 17:11:41 +0200

    before major refactoring

--- Chessboard.java
+++ Chessboard.java
@@ -1,43 +1,43 @@
 
 public class Chessboard {
 	private int[][] chessboard;
-	
+
 	public Chessboard(int rows, int columns) {
 		chessboard = new int[rows][columns];
 	}
 
 	public void initializeBoard() {
-		for(int i = 0; i < chessboard.length; i++) {
-			for(int j = 0; j < chessboard[i].length; j++) {
+		for (int i = 0; i < chessboard.length; i++) {
+			for (int j = 0; j < chessboard[i].length; j++) {
 				chessboard[i][j] = -1;
 			}
 		}
 	}
-	
+
 	public void printBoard() {
-		//a return for formatting reasons
+		// a return for formatting reasons
 		System.out.println("\n");
-		
-		//print the letters of the board first
-		for(int i = 0; i < chessboard.length; i++) {
-			System.out.print("\t#" + (char)(i + 65) + "#");
+
+		// print the letters of the board first
+		for (int i = 0; i < chessboard.length; i++) {
+			System.out.print("\t#" + (char) (i + 65) + "#");
 		}
-		
-		//another return
+
+		// another return
 		System.out.println("");
-		for(int i = 0; i < chessboard.length; i++) {
+		for (int i = 0; i < chessboard.length; i++) {
 			System.out.print("\n# " + (i + 1) + " #\t");
-			for(int j = 0; j < chessboard[i].length; j++) {
+			for (int j = 0; j < chessboard[i].length; j++) {
 				System.out.print(chessboard[i][j] + "\t");
 			}
 			System.out.println("");
 		}
 	}
-	
+
 	public void setQueen(int row, int column) {
 		chessboard[row][column] = 0;
 	}
-	
+
 	/**
 	 * 
 	 * @param rowCell_1
@@ -46,46 +46,152 @@ public class Chessboard {
 	 * @param columnCell_2
 	 * @return
 	 */
-	public boolean threat(int rowCell_1, int columnCell_1, int rowCell_2, int columnCell_2) {
-		boolean threatens = false;
-		
-		//vertical threat
-		if(rowCell_1 == rowCell_2) {
-			threatens = true;
-		}
-		
-		//horizontal threat
-		if(columnCell_1 == columnCell_2) {
-			threatens = true;
-		}
-		
-		//diagonal ascending right
-		try{
-			for(int i = 1; i < rowCell_2; i++) {
-				if(chessboard[rowCell_2 - i][columnCell_2 + i] == 0) {
-					threatens = true;
-				}
+	public boolean threat(int row, int column, int[][] chessboard) {
+
+		// horizontal threat
+		for (int i = column - 1; i >= 0; i--) {
+			if (chessboard[row][i] == 0) {
+				return true;
 			}
-		}catch(ArrayIndexOutOfBoundsException e) {
-			System.out.println("alles ok rechts");
-		}
-		
-		
-		try{
-			for(int i = 1; i < rowCell_2; i++) {
-				if(chessboard[rowCell_2 - i][columnCell_2 - i] == 0) {
-					threatens = true;
-				}
-			}
-		}catch(ArrayIndexOutOfBoundsException e) {
-			System.out.println("alles ok links");
 		}
 
-		
-		return threatens;
+		// vertical threat
+		for (int i = row - 1; i >= 0; i--) {
+			if (chessboard[i][column] == 0) {
+				return true;
+			}
+		}
+
+		// diagonal ascending left
+		int tmpColumn = column - 1;
+
+		for (int i = row - 1; i >= 0; i--) {
+
+			if (tmpColumn < 0) {
+				continue;
+			}
+			if (chessboard[i][tmpColumn] == 0) {
+				return true;
+			}
+
+			tmpColumn--;
+		}
+
+		// diagonal ascending right
+		tmpColumn = column + 1;
+
+		for (int i = row - 1; i >= 0; i--) {
+
+			if (tmpColumn == -1 || tmpColumn > 7) {
+				continue;
+			}
+			if (chessboard[i][tmpColumn] == 0) {
+				return true;
+			}
+
+			tmpColumn++;
+		}
+
+		return false;
 	}
 	
+	//coordinates for last queen for backtracking
+	int lastQueenRow;
+	int lastQueenColumn;
+
+	public int[][] findSolution(int[][] tempChessboard, int rowStart, int columnStart) {
+		
+		//little test
+//		if(chessboard[0][1] == 0) {
+//			System.out.println("Letztes Board erstmal");
+//			printBoard();
+//			return chessboard;
+//		}
+		// brauchen einen Solution Count
+		int queensSet = 0;
+
+		// erstmal termination condition
+		if (allSolutionsFound()) {
+			System.out.println("SUPPA!");
+			printBoard();
+			return chessboard;
+		} else {
+			for (int i = rowStart; i < chessboard.length; i++) {
+				for (int j = columnStart; j < chessboard.length; j++) {
+					
+					if(!threat(i, j, chessboard)) {
+						
+						System.out.println("Keine Bedrohung :)");
+						Queen queen = new Queen(i, j);
+						
+						setQueen(queen);
+						
+						queensSet++;
+						
+						findSolution(chessboard, queen.getRow() + 1, 0);
+					}
+					
+					System.out.println("Bedrohung!");
+				}
+			}
+		}
+		
+		printBoard();
+		
+		System.out.println("TRY BACKTRACKING");
+		findSolution(deleteLastQueenAndGetNewBoard(), lastQueenRow, lastQueenColumn + 1);
+		
+		System.out.println("VERSAGT");
+		return null;
+	}
 	
+	/**
+	 * find the last queen
+	 * set the field to -1
+	 * save the coordinates in the field variables lastQueenRow and lastQueenColumn
+	 * @return the new chessboard
+	 */
+	public int[][] deleteLastQueenAndGetNewBoard() {
+		System.out.println("BACKTRACKING STARTED");
+		for(int i = chessboard.length - 1; i >= 0; i--) {
+			System.out.println("Row checked");
+			for(int j = chessboard.length - 1; j >= 0 ; j--) {
+				if(chessboard[i][j] == 0) {
+					System.out.println("LAST QUEEN FOUND!");
+					chessboard[i][j] = -1;
+					lastQueenColumn = j;
+					lastQueenRow = i;
+					printBoard();
+					return chessboard;
+				}
+				System.out.println("Column checked");
+			}
+		}
+		
+		return null;
+	}
 	
+	public boolean allSolutionsFound() {
+		int amountsOfQueens = 0;
+		for(int i = 0; i < chessboard.length; i++) {
+			for(int j = 0; j < chessboard.length; j++) {
+				if(chessboard[i][j] == 0) {
+					amountsOfQueens++;
+				}
+				if(amountsOfQueens == chessboard.length) {
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
 	
+	public void setQueen(Queen queen) {
+		chessboard[queen.getRow()][queen.getColumn()] = 0;
+	}
+
+	public int[][] getBoard() {
+		return chessboard;
+	}
 }
--- Main.java
+++ Main.java
@@ -2,15 +2,22 @@
 public class Main {
 
 	public static void main(String[] args) {
+		Chessboard cb = new Chessboard(8, 8);
+		cb.initializeBoard();
+		cb.findSolution(cb.getBoard(), 0, 0);
+		cb.printBoard();
+	}
+	
+	public void testThreat() {
 		Chessboard chessboard = new Chessboard(8, 8);
-		Queen queen1 = new Queen(1, 6);
-		Queen queen2 = new Queen(3, 3);
+		Queen queen1 = new Queen(0, 3);
+		Queen queen2 = new Queen(1, 0);
 		chessboard.initializeBoard();
 		chessboard.printBoard();
 		chessboard.setQueen(queen1.getRow(), queen1.getColumn());
 		chessboard.setQueen(queen2.getRow(), queen2.getColumn());
 		chessboard.printBoard();
-		System.out.println("\nThreat: " + chessboard.threat(queen1.getRow(), queen1.getColumn(), queen2.getRow(), queen2.getColumn()));
+		System.out.println("\nThreat: " + chessboard.threat(queen2.getRow(), queen2.getColumn(), chessboard.getBoard()));
 	}
 
 }
